d2da2510799511a55d447fcd381cb948b4762fa9
zhaojianc <zhaojianc@yonyou.com>
2018-04-02 19:52:42 +0800

    add
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- pytest/src/neural/test.py
+++ pytest/src/neural/test.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 import numpy as np;
 
-print('使用列表生成一维数组')
-data = np.random.randn(100)
-print(data)
-print(data.mean())
-print(data.std())
+sizes = [2, 3, 1]
+biases = [np.random.randn(y, 1) for y in sizes[1:]]
+print("biases")
+print(biases)
+zipped = zip(sizes[:-1], sizes[1:])
+print("zipped")
+for z in zipped:
+    print(z)
+weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+print("weights")
+print(weights)
+for x, y in zip(sizes[:-1], sizes[1:]):
+    print(x, y)
