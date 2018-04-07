a8cbe63dd7d9a500c9c51e131460af2ca2acfb9c
zhaojian770627 <zhaojian770627@163.com>
2018-04-07 21:48:52 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- pytest/src/neural/mnist_loader.py
+++ pytest/src/neural/mnist_loader.py
@@ -42,3 +42,4 @@ def showimage(image):
 training_data, validation_data, test_data = load_data_wrapper()
 net = network.Network([784, 30, 10])
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+net.printbw()
