09c254e8e458df378e07a70832c7f2b273f8f7d9
zhaojian770627 <zhaojian770627@163.com>
2018-04-17 22:40:40 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ pytest/src/algorithm/gradient.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# 本文件演示下用梯度求方程的解
+
+# 求解方程 fx = x * x + 9
+
+
+def F(x):
+    return x * x - 9;
+
+
+# 导数
+def F_prime(x):
+    return 2 * x
+
+
+# 得到下一个X
+def Xn(x):
+    return x - eta * F_prime(x)
+
+
+def DELTA(x):
+    return -F_prime(x) * eta
+
+
+# 差值
+def E(xn, x):
+    return abs(xn - x)
+
+
+# 给x一个预设值
+# 学习速率
+eta = 0.001
+x = 10
+xn = 10
+e = 0.000001
+
+i = 0
+while True:
+    deltaX = DELTA(x)
+    if(abs(deltaX) < e):
+        break
+    i = i + 1
+#     if(i > 200):
+#         break ;
+    xn = x + deltaX
+    x = xn
+    print("Epoch {0}:x {1} xn {2}".format(i, x, xn))
+
+x = xn
+print("最优解 {0}".format(x))
