be6348f785a4f4d36f94adde93dfbc3100287735
zhaojianc <zhaojianc@yonyou.com>
2018-04-18 09:28:42 +0800

    up
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- pytest/src/algorithm/gradient.py
+++ pytest/src/algorithm/gradient.py
@@ -2,30 +2,23 @@
 # 本文件演示下用梯度求函数的极小值
 
 # 求解方程 fx = x * x + 9
-
-
 def F(x):
     return x * x + x;
-
 
 # 导数
 def F_prime(x):
     return 2 * x + 1
 
-
 # 得到下一个X
 def Xn(x):
     return x - eta * F_prime(x)
 
-
 def DELTA(x):
     return -F_prime(x) * eta
-
 
 # 差值
 def E(xn, x):
     return abs(xn - x)
-
 
 # 给x一个预设值
 # 学习速率
