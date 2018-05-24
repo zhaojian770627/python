d235f393333957dd49dc22e41f7273c368130406
zj <zj@zj-Lenovo>
2018-05-24 20:32:38 +0800

    up
    Signed-off-by: zj <zj@zj-Lenovo>

--- pytest/src/neural/test3.py
+++ pytest/src/neural/test3.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
 import random;
-
 import numpy as np;
+import network
 
-aw = np.array([[1, 2], [4, 5], [7, 8]])
-# A1[...]
-# B1[...]
-ax = np.array([[1, 2, 3], [1, 2, 3]])
-wx = np.dot(aw, ax)
-print(wx)
-b = np.array([1, 1, 1]).transpose();
-z = wx + b
-print(z)
+print('abc')
+b1=np.random.randn(1, 1);
+i=0;
