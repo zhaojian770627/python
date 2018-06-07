825664f06dd4aa00d40a612d37e6094053242c7b
zhaojian770627 <zhaojian770627@163.com>
2018-06-07 09:47:04 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- pytest/src/neural/test3.py
+++ pytest/src/neural/test3.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 import random;
 import numpy as np;
-
-num_epochs = int(input("Enter the number of epochs to run for: "))
+import network2
+import mnist_loader
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+net = network2.Network([784, 30, 10])
+test_cost, test_accuracy, training_cost, training_accuracy \
+ = net.SGD(training_data, 30, 10, .05, lmbda=50.0, evaluation_data=None, monitor_evaluation_accuracy=False, monitor_training_cost=True)
+print(training_cost)
