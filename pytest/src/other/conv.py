fc7437748c63c33c009cc52c037dc0f59f99a226
zhaojianc <zhaojianc@yonyou.com>
2018-08-10 09:56:02 +0800

    add
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- pytest/src/other/conv.py
+++ pytest/src/other/conv.py
@@ -1,5 +1,5 @@
 import numpy as np
-x = np.array([1, 2, 3])
-h = np.array([4, 5, 6])
+x = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+h = np.array([[1, 1], [1, 1]])
 import scipy.signal
 print(scipy.signal.convolve(x, h))  # 卷积运算
