bf82f32313c2c99d9cb1af932fabd13273bda3b6
zhaojian770627 <zhaojian770627@163.com>
2018-08-10 22:00:01 +0800

    aaa
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- pytest/src/other/conv2.py
+++ pytest/src/other/conv2.py
@@ -1,16 +1,32 @@
-import pickle
 import gzip
+import pickle
 
-# Third-party libraries
-import numpy as np
 import theano
-import theano.tensor as T
-# from theano.tensor.nnet import conv
+from theano.tensor import shared_randomstreams
 from theano.tensor.nnet import conv2d
 from theano.tensor.nnet import softmax
-from theano.tensor import shared_randomstreams
 from theano.tensor.signal.pool import pool_2d
 
+import numpy as np
+import theano.tensor as T
+
+# 卷积及混合的测试
+# Third-party libraries
+# from theano.tensor.nnet import conv
 d = T.dmatrix("d")
-x = np.random.random((28, 28))
-print(x)
+filter_shape = (1, 1, 5, 5)
+image_shape = (1, 1, 6, 6)
+poolsize = (2, 2)
+x = T.matrix("x")
+n_out = (filter_shape[0] * np.prod(filter_shape[2:]) / np.prod(poolsize))
+a = np.asarray(np.random.randint(low=1, high=2, size=(5, 5)))
+print(a)
+w = theano.shared(a.reshape(filter_shape), borrow=True)
+inpt = x.reshape(image_shape)
+conv_out = conv2d(input=inpt, filters=w, filter_shape=filter_shape, input_shape=image_shape)
+print(theano.printing.debugprint(conv_out))
+theano.printing.pydotprint(conv_out, "/home/zj/a.png")
+f = theano.function([x], conv_out)
+rin = np.random.randint(low=1, high=5, size=(6, 6))
+print(rin)
+print(f(rin))
