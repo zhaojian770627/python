703aaf2014cf8ed9e1193a2849b77fc6c317c343
zhaojian770627 <zhaojian770627@163.com>
2018-08-13 22:33:58 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- pytest/src/action/kNN.py
+++ pytest/src/action/kNN.py
@@ -16,10 +16,14 @@ def classify0(inX, dataSet, labels, k):
     sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances ** .5
     sortedDistIndicies = distances.argsort()
-    classCount = ()
+    classCount = {}
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
-    sortedClassCount = sorted(classCount.iteritems(),
+    sortedClassCount = sorted(classCount.items(),
         key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]    
+
+
+group, labels = createDataSet()
+print(classify0([0, 0], group, labels, 3))
