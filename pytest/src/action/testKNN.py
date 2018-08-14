cebf5886a59d4597bfec1c0f4cab477c9de046a1
zhaojianc <zhaojianc@yonyou.com>
2018-08-14 13:56:18 +0800

    add
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- pytest/src/action/testKNN.py
+++ pytest/src/action/testKNN.py
@@ -1,7 +1,12 @@
+import matplotlib
+import matplotlib.pyplot as plt
 import kNN
 
-group, labels = kNN.createDataSet()
-print(kNN.classify0([0, 0], group, labels, 3))
+# group, labels = kNN.createDataSet()
+# print(kNN.classify0([0, 0], group, labels, 3))
 
-datingDataMat, datingLabels = kNN.file2matrix('/home/zj/datingTestSet.txt')
-print(datingLabels)
+datingDataMat, datingLabels = kNN.file2matrix('/home/zj/datingTestSet2.txt')
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2])
+plt.show()
