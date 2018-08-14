36b6c0f6b1f865c9378762153d5bac6a1fb05f92
zhaojianc <zhaojianc@yonyou.com>
2018-08-14 20:39:38 +0800

    add
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- pytest/src/action/testKNN.py
+++ pytest/src/action/testKNN.py
@@ -4,14 +4,18 @@ from numpy import *
 import kNN
 import matplotlib.pyplot as plt
 
+
 # group, labels = kNN.createDataSet()
 # print(kNN.classify0([0, 0], group, labels, 3))
-datingDataMat, datingLabels = kNN.file2matrix('/home/zj/datingTestSet2.txt')
-fig = plt.figure()
-ax = fig.add_subplot(111)
-# 参数 x,y,伸缩率,颜色
-ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
-plt.title('Sample')  # 显示图表标题
-plt.xlabel('x')  # x轴名称
-plt.ylabel('y')  # y轴名称
-plt.show()
+def a():
+    datingDataMat, datingLabels = kNN.file2matrix('/home/zj/datingTestSet2.txt')
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    # ax.scatter(datingDataMat[:,1], datingDataMat[:,2])
+    ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
+    ax.axis([-2, 25, -0.2, 2.0])
+    plt.xlabel('Percentage of Time Spent Playing Video Games')
+    plt.ylabel('Liters of Ice Cream Consumed Per Week')
+    plt.show()
+
+a()
