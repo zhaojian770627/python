5b728ea4873521fc1856b1e6b48feee899dde6be
zhaojianc <zhaojianc@yonyou.com>
2018-08-14 16:36:22 +0800

    add
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- pytest/src/action/testKNN.py
+++ pytest/src/action/testKNN.py
@@ -9,5 +9,6 @@ import kNN
 datingDataMat, datingLabels = kNN.file2matrix('/home/zj/datingTestSet2.txt')
 fig = plt.figure()
 ax = fig.add_subplot(111)
+# 参数 x,y,伸缩率,颜色
 ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
 plt.show()
