dabfe93f47c8eaf20503ba66086dc0d1a888868a
zhaojianc <zhaojianc@yonyou.com>
2018-08-14 10:19:17 +0800

    up
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- pytest/src/action/kNN.py
+++ pytest/src/action/kNN.py
@@ -8,6 +8,22 @@ def createDataSet():
     return group, labels
 
 
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    
+    return returnMat, classLabelVector
+
 # k-近邻算法
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
