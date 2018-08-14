0d987eb022e349a6e5ddfaca41516b9cfa5587fb
zhaojianc <zhaojianc@yonyou.com>
2018-08-14 14:23:55 +0800

    散列点
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- pytest/src/action/testKNN.py
+++ pytest/src/action/testKNN.py
@@ -1,3 +1,4 @@
+from numpy import *
 import matplotlib
 import matplotlib.pyplot as plt
 import kNN
@@ -8,5 +9,5 @@ import kNN
 datingDataMat, datingLabels = kNN.file2matrix('/home/zj/datingTestSet2.txt')
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2])
+ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
 plt.show()
