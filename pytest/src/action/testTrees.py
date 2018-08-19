5d15b76b1dc4d4e573455407bc875d4d8f82ae25
zhaojian770627 <zhaojian770627@163.com>
2018-08-19 11:25:13 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- pytest/src/action/testTrees.py
+++ pytest/src/action/testTrees.py
@@ -6,5 +6,6 @@ import matplotlib.pyplot as plt
 
 myDat, labels = trees.createDataSet()
 print(myDat)
-print(trees.splitDataSet(myDat, 0, 1))
-print(trees.chooseBestFeatureToSplit(myDat))
+# print(trees.splitDataSet(myDat, 0, 1))
+# print(trees.chooseBestFeatureToSplit(myDat))
+print(trees.createTree(myDat,labels))
--- pytest/src/action/trees.py
+++ pytest/src/action/trees.py
@@ -1,5 +1,5 @@
 from math import log
-from _functools import reduce
+import operator
 
 
 def createDataSet():
@@ -61,3 +61,34 @@ def chooseBestFeatureToSplit(dataSet):
             bestFeature = i
     
     return bestFeature
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():classCount[vote] = 0
+        classCount[vote] += 1
+    
+    sortedClassCount = sorted(classCount.items, key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+# 创建树
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    # 类别完全相同则停止继续划分
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    # 遍历完所有特征时返回出现次数最多的
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList);
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
