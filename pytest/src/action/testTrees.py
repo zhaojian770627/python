a516ed2692b1833e6777462df6fba3ab659316ca
zhaojian770627 <zhaojian770627@163.com>
2018-08-20 22:13:22 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- pytest/src/action/testTrees.py
+++ pytest/src/action/testTrees.py
@@ -1,9 +1,10 @@
 import trees
 import treePlotter
 
-myDat, labels = trees.createDataSet()
-myTree = treePlotter.retrieveTree(0)
-trees.storeTree(myTree, 'tree.txt')
-myTree2 = trees.grabTree('tree.txt')
-print(myTree2)
+fr = open("lenses.txt")
+lenses = [ inst.strip().split('\t') for inst in fr.readlines()]
+lensesLabels = ['age', 'prescript', 'astigmatic', 'tearRate']
+lensesTree = trees.createTree(lenses, lensesLabels)
+print(lensesTree)
+treePlotter.createPlot(lensesTree)
 
