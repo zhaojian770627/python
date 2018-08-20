4575b21c0375c56f576189fe7d0040c5f53481b9
zhaojian770627 <zhaojian770627@163.com>
2018-08-20 19:58:26 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- pytest/src/action/trees.py
+++ pytest/src/action/trees.py
@@ -92,3 +92,16 @@ def createTree(dataSet, labels):
         subLabels = labels[:]
         myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
     return myTree
+
+
+def classify(inputTree, featLabels, testVec):
+    firstStr = inputTree.keys()[0]
+    secondDict = inputTree(firstStr)
+    featIndex = featLabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__ == 'dict':
+                classLabel = classify(secondDict[key], featLabels, testVec)
+            else:
+                classLabel = secondDict[key]
+    return classLabel
