b66103ab46741d0ef31db95c81346de7e313bc66
zhaojianc <zhaojianc@yonyou.com>
2018-08-21 19:04:33 +0800

    add
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- pytest/src/action/testBayes.py
+++ pytest/src/action/testBayes.py
@@ -8,4 +8,9 @@ for postinDoc in listOPosts:
     trainMat.append(bayes.setOfWords2Vec(myVocabList, postinDoc))
     
 p0V, p1V, pAb = bayes.trainNB0(trainMat, listClasses)
+print('-------------------p0V----------------------')
+print(p0V)
+print('-------------------p1V----------------------')
+print(p1V)
+print('-------------------pAb----------------------')
 print(pAb)
