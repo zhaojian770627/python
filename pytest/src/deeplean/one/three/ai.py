bc4b5a84c9ade5b82bae07dd1ce6323b8fec296d
zhaojian770627 <zhaojian770627@163.com>
2019-06-02 16:42:24 +0800

    up
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ pytest/src/deeplean/one/three/ai.py
@@ -0,0 +1,26 @@
+# Package imports
+import numpy as np
+import matplotlib.pyplot as plt
+from testCases import *
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+
+# matplotlib inline
+
+np.random.seed(1)  # set a seed so that the results are consistent
+X, Y = load_planar_dataset() 
+plt.scatter(X[0, :], X[1, :], c=Y[0, :], s=40, cmap=plt.cm.Spectral);
+
+clf = sklearn.linear_model.LogisticRegressionCV();
+clf.fit(X.T, Y.T);
+
+# Plot the decision boundary for logistic regression
+plot_decision_boundary(lambda x: clf.predict(x), X, Y)
+plt.title("Logistic Regression")
+
+# Print accuracy
+LR_predictions = clf.predict(X.T)
+print ('Accuracy of logistic regression: %d ' % float((np.dot(Y,LR_predictions) + np.dot(1-Y,1-LR_predictions))/float(Y.size)*100) +
+       '% ' + "(percentage of correctly labelled datapoints)")
