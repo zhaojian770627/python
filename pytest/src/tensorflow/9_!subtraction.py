90becfa192fdf0e5fa14b4b9289278caabcb1792
zhaojian770627 <zhaojian770627@163.com>
2019-10-27 16:54:18 +0800

    RNN网络2

--- pytest/src/tensorflow/9_!subtraction.py
+++ pytest/src/tensorflow/9_!subtraction.py
@@ -20,4 +20,21 @@ binary = np.unpackbits(np.array([range(largest_number)], dtype=np.uint8).T, axis
 
 for i in range(largest_number):
     int2binary[i] = binary[i]
+
+# 参数设置
+alpha = 0.9  # 学习速率
+input_dim = 2  # 输入的维度是2，减数和被减数
+hidden_dim = 16
+output_dim = 1  # 输出维度为1
+
+# 初始化网络
+synapse_0 = (2 * np.random.random((input_dim, hidden_dim)) - 1) * .05  # 维度为2*16,2是输入维度，16是隐藏层维度
+synapse_1 = (2 * np.random.random((hidden_dim, output_dim)) - 1) * .05
+synapse_h = (2 * np.random.reandom((hidden_dim, hidden_dim)) - 1) * .05
+
+# 用于存放反向传播的权重更新值
+synapse_0_update = np.zeros_like(synapse_0)
+synapse_1_update = np.zeros_like(synapse_1)
+synapse_h_update = np.zeros_like(synapse_h)
+
 print(len(int2binary))
